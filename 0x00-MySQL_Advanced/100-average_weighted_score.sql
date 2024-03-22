94a32eed232f8e8ef59533ad6fcd8763dbb7edeb
Mohamed AJJAJI <majjaji98@gmail.com>
2024-03-22 01:17:23 +0000

    Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,36 @@
+-- Creates a stored procedure ComputeAverageWeightedScoreForUser that computes
+-- and store the average weighted score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
+BEGIN
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
+
+    -- Calculate total weighted score for the user
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+        INNER JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    -- Calculate total weight for the user
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+        INNER JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    -- Update average_score for the user based on total weighted score and total weight
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
+END;
+//
+DELIMITER ;
